34f1714e7ad72dfeaf4eac73d9cb930830df5e0a
Jack Mitchell <jack@embed.me.uk>
2013-09-03 14:41:38 +0100

    improve test_gpio fork semantics

--- test/gpio_test.c
+++ test/gpio_test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #include "libsoc_gpio.h"
 
@@ -171,6 +172,10 @@ int main(void)
   {
     printf("No interrupt triggered!\n"); 
   }
+  
+  int status;
+  
+  wait(&status);
   
   fail:
   
