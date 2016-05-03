e5dcef5de91a206520bbfc9cac847ba634bcfe75
Jack Mitchell <jack@embed.me.uk>
2016-05-03 13:49:40 +0100

    tests: try to cover all different --enable-python options

--- test/test_configure.sh
+++ test/test_configure.sh
@@ -31,18 +31,73 @@ autoreconf -i
 make
 make clean
 
+./configure ${CONFIGURE_FLAGS} --prefix=$(pwd)/test/tmpinstall
+make
+make install
+find test/ -name *python* | grep -qz . && { echo "python found when not configured"; exit 1; rm -r test/tmpinstall; }
+rm -r test/tmpinstall
+make clean
+
+./configure ${CONFIGURE_FLAGS} --enable-python=no --prefix=$(pwd)/test/tmpinstall
+make
+make install
+find test/ -name *python* | grep -qz . && { echo "python found when set to no"; exit 1; rm -r test/tmpinstall; }
+rm -r test/tmpinstall
+make clean
+
+if ./configure ${CONFIGURE_FLAGS} --enable-python=/usr/bin/python-nope ; then
+	echo "configure succeeded when given bad python path"
+	exit 1
+fi
+
 if [ $python2 -ne 0 ]; then
-	./configure ${CONFIGURE_FLAGS} #--enable-python=2
+
+	./configure ${CONFIGURE_FLAGS} --enable-python --prefix=$(pwd)/test/tmpinstall
 	make
-	test -e ../bindings/python/gpio_python.lo || echo "python2 build failed" && exit 1
+	make install
+	find test/ -name *python* | grep -qz . || { echo "python auto failed"; exit 1; rm -r test/tmpinstall; }
+	rm -r test/tmpinstall
 	make clean
+
+	./configure ${CONFIGURE_FLAGS} --enable-python=2 --prefix=$(pwd)/test/tmpinstall
+	make
+	make install
+	find test/ -name *python2* | grep -qz . || { echo "python2 version build failed"; exit 1; rm -r test/tmpinstall; }
+	rm -r test/tmpinstall
+	make clean
+
+	./configure ${CONFIGURE_FLAGS} --enable-python=/usr/bin/python2 --prefix=$(pwd)/test/tmpinstall
+	make
+	make install
+	find test/ -name *python2* | grep -qz . || { echo "python2 full path build failed"; exit 1; rm -r test/tmpinstall; }
+	rm -r test/tmpinstall
+	make clean
+
 fi
 
 if [ $python3 -ne 0 ]; then
-	./configure ${CONFIGURE_FLAGS} --enable-python=3
+
+	./configure ${CONFIGURE_FLAGS} --enable-python --prefix=$(pwd)/test/tmpinstall
 	make
-	test -e ../bindings/python/gpio_python.lo || echo "python3 build failed" && exit 1
+	make install
+	find test/ -name *python* | grep -qz . || { echo "python auto build failed"; exit 1; rm -r test/tmpinstall; }
+	rm -r test/tmpinstall
 	make clean
+
+	./configure ${CONFIGURE_FLAGS} --enable-python=3 --prefix=$(pwd)/test/tmpinstall
+	make
+	make install
+	find test/ -name *python3* | grep -qz . || { echo "python3 version only build failed"; exit 1; rm -r test/; }
+	rm -r test/tmpinstall
+	make clean
+
+	./configure ${CONFIGURE_FLAGS} --enable-python=/usr/bin/python3 --prefix=$(pwd)/test/tmpinstall
+	make
+	make install
+	find test/ -name *python3* | grep -qz . || { echo "python3 full path build failed"; exit 1; rm -r test/; }
+	rm -r test/tmpinstall
+	make clean
+
 fi
 
 ./configure ${CONFIGURE_FLAGS} --enable-board=beaglebone_black
@@ -56,3 +111,5 @@ make clean
 ./configure ${CONFIGURE_FLAGS} --disable-debug
 make
 make clean
+
+echo -e "\n\n\n\n.................. SUCCESS ...................\n\n\n\n"
