406c4c95aedb3041abf2017f5f116c42200cea8b
Rail Shafigulin <rshafigulin@fb.com>
2017-05-23 17:11:55 -0700

    Some indentation and whitespace fixes. Nothign major

--- lib/spi.c
+++ lib/spi.c
@@ -19,27 +19,27 @@ libsoc_spi_debug (const char *func, spi * spi, char *format, ...)
 #ifdef DEBUG
 
   if (libsoc_get_debug ())
+  {
+    va_list args;
+
+    fprintf (stderr, "libsoc-spi-debug: ");
+
+    va_start (args, format);
+    vfprintf (stderr, format, args);
+    va_end (args);
+
+    if (spi == NULL)
     {
-      va_list args;
-
-      fprintf (stderr, "libsoc-spi-debug: ");
-
-      va_start (args, format);
-      vfprintf (stderr, format, args);
-      va_end (args);
-
-      if (spi == NULL)
-  {
-    fprintf (stderr, " (NULL, %s)", func);
-  }
-      else
-  {
-    fprintf (stderr, " (spidev%d.%d, %s)", spi->spi_dev,
-       spi->chip_select, func);
-  }
-
-      fprintf (stderr, "\n");
+      fprintf (stderr, " (NULL, %s)", func);
     }
+    else
+    {
+      fprintf (stderr, " (spidev%d.%d, %s)", spi->spi_dev,
+          spi->chip_select, func);
+    }
+
+    fprintf (stderr, "\n");
+  }
 #endif
 }
 
@@ -49,15 +49,15 @@ libsoc_spi_init (uint16_t spidev_device, uint8_t chip_select)
   spi *spi_dev;
 
   libsoc_spi_debug (__func__, NULL, "initialising spidev device %d.%d",
-        spidev_device, chip_select);
+      spidev_device, chip_select);
 
   spi_dev = malloc (sizeof (spi));
 
   if (spi_dev == NULL)
-    {
-      libsoc_spi_debug (__func__, NULL, "failed to allocate memory");
-      return NULL;
-    }
+  {
+    libsoc_spi_debug (__func__, NULL, "failed to allocate memory");
+    return NULL;
+  }
 
   char path[42];
 
@@ -67,18 +67,18 @@ libsoc_spi_init (uint16_t spidev_device, uint8_t chip_select)
   sprintf (path, "/dev/spidev%d.%d", spi_dev->spi_dev, spi_dev->chip_select);
 
   if (!file_valid (path))
-    {
-      libsoc_spi_debug (__func__, spi_dev, "%s not a vaild device", path);
-      goto error;
-    }
+  {
+    libsoc_spi_debug (__func__, spi_dev, "%s not a vaild device", path);
+    goto error;
+  }
 
   spi_dev->fd = file_open (path, O_SYNC | O_RDWR);
 
   if (spi_dev->fd < 0)
-    {
-      libsoc_spi_debug (__func__, spi_dev, "%s could not be opened", path);
-      goto error;
-    }
+  {
+    libsoc_spi_debug (__func__, spi_dev, "%s could not be opened", path);
+    goto error;
+  }
 
   return spi_dev;
 
@@ -93,21 +93,21 @@ int
 libsoc_spi_set_bits_per_word (spi * spi, spi_bpw bpw)
 {
   if (bpw != BITS_8 && bpw != BITS_16)
-    {
-      libsoc_spi_debug (__func__, spi, "bits per word was not BITS_8"
-      " or BITS_16", bpw);
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "bits per word was not BITS_8"
+        " or BITS_16", bpw);
+    return EXIT_FAILURE;
+  }
 
   libsoc_spi_debug (__func__, spi, "setting bits per word to %d", bpw);
 
   int ret = ioctl (spi->fd, SPI_IOC_WR_BITS_PER_WORD, &bpw);
 
   if (ret == -1)
-    {
-      libsoc_spi_debug (__func__, spi, "failed setting bits per word");
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "failed setting bits per word");
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
@@ -120,13 +120,13 @@ libsoc_spi_get_bits_per_word (spi * spi)
   int ret = ioctl (spi->fd, SPI_IOC_RD_BITS_PER_WORD, &bpw);
 
   if (ret == -1)
-    {
-      libsoc_spi_debug (__func__, spi, "failed reading bits per word");
-      return BPW_ERROR;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "failed reading bits per word");
+    return BPW_ERROR;
+  }
 
   switch (bpw)
-    {
+  {
     case 8:
       libsoc_spi_debug (__func__, spi, "read bits per word as 8");
       return BITS_8;
@@ -136,7 +136,7 @@ libsoc_spi_get_bits_per_word (spi * spi)
     default:
       libsoc_spi_debug (__func__, spi, "bits per word not recognised");
       return BPW_ERROR;
-    }
+  }
 }
 
 int
@@ -147,10 +147,10 @@ libsoc_spi_set_speed (spi * spi, uint32_t speed)
   int ret = ioctl (spi->fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
 
   if (ret == -1)
-    {
-      libsoc_spi_debug (__func__, spi, "failed setting speed");
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "failed setting speed");
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
@@ -163,10 +163,10 @@ libsoc_spi_get_speed (spi * spi)
   int ret = ioctl (spi->fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
 
   if (ret == -1)
-    {
-      libsoc_spi_debug (__func__, spi, "failed reading speed");
-      return -1;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "failed reading speed");
+    return -1;
+  }
 
   libsoc_spi_debug (__func__, spi, "read speed as %dHz", speed);
 
@@ -177,17 +177,17 @@ int
 libsoc_spi_set_mode (spi * spi, spi_mode mode)
 {
   if (spi == NULL)
-    {
-      libsoc_spi_debug (__func__, NULL, "spi was not valid");
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, NULL, "spi was not valid");
+    return EXIT_FAILURE;
+  }
 
   libsoc_spi_debug (__func__, spi, "setting mode to %d", mode);
 
   uint8_t new_mode;
 
   switch (mode)
-    {
+  {
     case MODE_0:
       new_mode = SPI_MODE_0;
       break;
@@ -202,15 +202,15 @@ libsoc_spi_set_mode (spi * spi, spi_mode mode)
       break;
     default:
       return EXIT_FAILURE;
-    }
+  }
 
   int ret = ioctl (spi->fd, SPI_IOC_WR_MODE, &new_mode);
 
   if (ret == -1)
-    {
-      libsoc_spi_debug (__func__, spi, "failed setting mode");
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "failed setting mode");
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
@@ -221,21 +221,21 @@ libsoc_spi_get_mode (spi * spi)
   uint8_t mode;
 
   if (spi == NULL)
-    {
-      libsoc_spi_debug (__func__, NULL, "spi was not valid");
-      return MODE_ERROR;
-    }
+  {
+    libsoc_spi_debug (__func__, NULL, "spi was not valid");
+    return MODE_ERROR;
+  }
 
   int ret = ioctl (spi->fd, SPI_IOC_RD_MODE, &mode);
 
   if (ret == -1)
-    {
-      libsoc_spi_debug (__func__, spi, "failed reading mode");
-      return MODE_ERROR;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "failed reading mode");
+    return MODE_ERROR;
+  }
 
   switch (mode)
-    {
+  {
     case SPI_MODE_0:
       libsoc_spi_debug (__func__, spi, "read mode as 0");
       return MODE_0;
@@ -251,26 +251,26 @@ libsoc_spi_get_mode (spi * spi)
     default:
       libsoc_spi_debug (__func__, spi, "mode not recognised");
       return MODE_ERROR;
-    }
+  }
 }
 
 int
 libsoc_spi_write (spi * spi, uint8_t * tx, uint32_t len)
 {
   libsoc_spi_debug (__func__, spi, "performing write transfer of %d bytes",
-        len);
+      len);
 
   if (spi == NULL || tx == NULL)
-    {
-      libsoc_spi_debug (__func__, spi, "spi or tx was NULL");
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "spi or tx was NULL");
+    return EXIT_FAILURE;
+  }
 
   if (len <= 0)
-    {
-      libsoc_spi_debug (__func__, spi, "length was less than zero");
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "length was less than zero");
+    return EXIT_FAILURE;
+  }
 
   int ret;
 
@@ -294,19 +294,19 @@ int
 libsoc_spi_read (spi * spi, uint8_t * rx, uint32_t len)
 {
   libsoc_spi_debug (__func__, spi, "performing read transfer of %d bytes",
-        len);
+      len);
 
   if (spi == NULL || rx == NULL)
-    {
-      libsoc_spi_debug (__func__, spi, "spi or rx was NULL");
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "spi or rx was NULL");
+    return EXIT_FAILURE;
+  }
 
   if (len <= 0)
-    {
-      libsoc_spi_debug (__func__, spi, "length was less than zero");
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "length was less than zero");
+    return EXIT_FAILURE;
+  }
 
   int ret;
 
@@ -318,10 +318,10 @@ libsoc_spi_read (spi * spi, uint8_t * rx, uint32_t len)
   ret = ioctl (spi->fd, SPI_IOC_MESSAGE (1), &tr);
 
   if (ret < 1)
-    {
-      libsoc_spi_debug (__func__, spi, "failed recieving message");
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "failed recieving message");
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
@@ -330,19 +330,19 @@ int
 libsoc_spi_rw (spi * spi, uint8_t * tx, uint8_t * rx, uint32_t len)
 {
   libsoc_spi_debug (__func__, spi,
-        "performing duplex rw transfer of %d bytes", len);
+      "performing duplex rw transfer of %d bytes", len);
 
   if (spi == NULL || rx == NULL || tx == NULL)
-    {
-      libsoc_spi_debug (__func__, spi, "spi | rx | tx was NULL");
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "spi | rx | tx was NULL");
+    return EXIT_FAILURE;
+  }
 
   if (len <= 0)
-    {
-      libsoc_spi_debug (__func__, spi, "length was less than zero");
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, spi, "length was less than zero");
+    return EXIT_FAILURE;
+  }
 
   int ret;
 
@@ -367,10 +367,10 @@ int
 libsoc_spi_free (spi * spi)
 {
   if (spi == NULL)
-    {
-      libsoc_spi_debug (__func__, NULL, "spi was not valid");
-      return EXIT_FAILURE;
-    }
+  {
+    libsoc_spi_debug (__func__, NULL, "spi was not valid");
+    return EXIT_FAILURE;
+  }
 
   libsoc_spi_debug (__func__, spi, "freeing spi device");
 
