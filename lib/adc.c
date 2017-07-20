77cca37991051f9e07eba9963ed7601e7d2adb83
John Comeau <jc@unternet.net>
2017-07-20 08:56:56 -0700

    use the correct functions

--- lib/adc.c
+++ lib/adc.c
@@ -96,18 +96,19 @@ adc* libsoc_adc_request (unsigned int chip, unsigned int adc_num)
 int libsoc_adc_get_value (adc *adc)
 {
   char value[64] = "";  // only 12 bit number but anticipating future needs
-  char *result;
+  int result;
   if (adc == NULL)
   {
     libsoc_adc_debug(__func__, -1, -1, "invalid adc pointer");
     return -1;
   }
-  result = fgets(value, 64, adc->fd);
-  if (result != value || strlen(value) < 1)
+  result = file_read(adc->fd, value, 64);
+  if (result < 0 || strlen(value) < 1 || strlen(value) > 64)
   {
     libsoc_adc_debug(__func__, adc->chip, adc->adc, "ADC read failure");
     return -1;
   }
+  libsoc_adc_debug(__func__, adc->chip, adc->adc, "ADC value: \"%s\"", value);
   return atoi(value);
 }
 
