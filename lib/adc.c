770a8362d5ef6e878544a37f52d9d29e0a2b41c1
John Comeau <jc@unternet.net>
2017-07-20 07:10:25 -0700

    about to test adc, copied from pwm sources and modified

---
+++ lib/adc.c
@@ -0,0 +1,112 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#include "libsoc_file.h"
+#include "libsoc_debug.h"
+#include "libsoc_adc.h"
+
+#define STR_BUF 256
+
+void libsoc_adc_debug (const char *func, unsigned int chip,
+  unsigned int adc, char *format, ...)
+{
+#ifdef DEBUG
+  if (libsoc_get_debug ())
+  {
+    va_list args;
+
+    fprintf (stderr, "libsoc-adc-debug: ");
+
+    va_start (args, format);
+    vfprintf (stderr, format, args);
+    va_end (args);
+
+    fprintf (stderr, " ((%d,%d), %s)", chip, adc, func);
+
+    fprintf (stderr, "\n");
+  }
+#endif
+}
+
+adc* libsoc_adc_request (unsigned int chip, unsigned int adc_num);
+{
+  adc *new_adc;
+  char chip_str[STR_BUF];
+  char adc_str[STR_BUF];
+  int i, j;
+  size_t result;
+
+  libsoc_adc_debug (__func__, chip, adc_num, "requested ADC");
+
+  for (i = 0; i < sizeof(ADC_DEVICE_FORMAT); i++)
+  {
+    result = snprintf(chip_str, STR_BUF, ADC_DEVICE_FORMAT[i], chip);
+    if (result < 0)
+    {
+      perror("Failed formatting chip string");
+      return NULL;
+    }
+    if (result >= STR_BUF)
+    {
+      fprintf(stderr, "Chip format string \"%s\" too long for buffer\n",
+              ADC_DEVICE_FORMAT[i]);
+      return NULL;
+    }
+    if (file_valid(chip_str))
+    {
+      libsoc_adc_debug(__func__, chip, adc_num, "Found requested chip");
+      for (j = 0; j < sizeof(ADC_READ_FORMAT), j++)
+      {
+        strncpy(adc_str, chip_str, STR_BUF);
+        strncpy(adc_str + result, ADC_READ_FORMAT[j],
+                STR_BUF - strlen(adc_str));
+        if (strlen(adc_str) > STR_BUF)
+        {
+          perror("Failed formatting device string");
+          return NULL;
+        }
+        if (file_valid(adc_str))
+        {
+          libsoc_adc_debug(__func__, chip, adc_num, "Found requested ADC pin");
+          new_adc = malloc(sizeof(adc));
+          new_adc->fd = file_open(adc_str, O_RDONLY);
+	}
+        if (new_adc->fd < 0)
+        {
+          free(new_pwm);
+          libsoc_pwm_debug(__func__, chip, adc_num,
+            "Failed to open adc sysfs file: %d", new_adc->fd);
+          return NULL;
+        }
+        new_adc->chip = chip;
+        new_adc->adc = adc_num;
+        return new_adc;
+      }
+    }
+  }
+}
+
+int libsoc_adc_free(adc *adc)
+{
+  if (adc == NULL)
+  {
+    libsoc_adc_debug(__func__, -1, -1, "invalid adc pointer");
+    return EXIT_FAILURE;
+  }
+
+  if (file_close(adc->fd) < 0)
+  {
+    return EXIT_FAILURE;
+  }
+
+  free(adc);
+  return EXIT_SUCCESS;
+}
+/*
+ * vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
+ */
---
+++ lib/include/libsoc_adc.h
@@ -0,0 +1,59 @@
+#ifndef _LIBSOC_ADC_H_
+#define _LIBSOC_ADC_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static const char ADC_DEVICE_FORMAT[][] = {
+	"/sys/devices/12d10000.adc/iio:device%d",  // ARTIK 1020
+	"/sys/devices/platform/c0000000.soc/c0053000.adc/iio:device%d", // 710
+	"/sys/devices/126c0000.adc/iio:device%d"  // ARTIK 520
+};
+
+static const char ADC_READ_FORMAT[][] = {
+	"/in_voltage%d_raw"
+};
+
+/**
+ * \struct adc 
+ * \brief representation of a single requested adc
+ * \param unsigned int adc - adc num
+ * \param unsigned int adc_chip - adc chip num
+ * \param int fd - file descriptor to adc file
+ */
+
+typedef struct {
+	unsigned int chip;
+	unsigned int adc;
+	int fd;
+} adc;
+
+/**
+ * \fn adc* libsoc_adc_request(unsigned int adc_chip, unsigned int adc_num)
+ * \brief request an adc to use
+ * \param unsigned int adc_chip - the chip number that controls the adc you
+    wish to use
+ * \param unsigned int adc_num - the adc number number within your adc chip
+ * \return pointer to adc* on success NULL on fail
+ *
+ */
+
+adc* libsoc_adc_request(unsigned int adc_chip, unsigned int adc_num);
+
+/**
+ * \fn int libsoc_adc_free(adc* adc)
+ * \brief free a previously requested adc
+ * \param adc* adc - valid pointer to a requested adc
+ * \return EXIT_SUCCESS or EXIT_FAILURE
+ */
+
+int libsoc_adc_free(adc *adc);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+/*
+ * vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
+ */
