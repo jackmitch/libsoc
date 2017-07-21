a454c6236f21b4c8f06f6e75b2cb2988242eae5c
John Comeau <jc@unternet.net>
2017-07-20 17:35:23 -0700

    got bit by the failure to "open" again

--- test/pwm_test.py
+++ test/pwm_test.py
@@ -16,23 +16,23 @@ def test_pwm(chip=0, pin=1, mode='shared', polarity='normal', period=10,
     Duplicate pwm_test.c
     '''
     kwargs = {k: safe_int(v) for k, v in locals().items()}
-    pwm = PWM(**kwargs)
-    for key in kwargs:
-        value = getattr(pwm, key)
-        if value != kwargs[key]:
-            sys.stderr.write('Wrong value %s for pwm.%s\n' % (value, key))
-    pwm.duty_cycle = pwm.period + 5
-    if pwm.duty_cycle == pwm.period + 5:
-        logging.error('PWM allowed a duty cycle > 100%')
-    else:
-        logging.debug('PWM properly rejected >100% duty cycle')
-        pass
-    pwm.on()
-    if not pwm.enabled:
-        logging.error('The PWM would not turn on')
-    pwm.off()
-    if pwm.enabled:
-        logging.error('The PWM would not shut off')
+    with PWM(**kwargs) as pwm:
+        for key in kwargs:
+            value = getattr(pwm, key)
+            if value != kwargs[key]:
+                sys.stderr.write('Wrong value %s for pwm.%s\n' % (value, key))
+        pwm.duty_cycle = pwm.period + 5
+        if pwm.duty_cycle == pwm.period + 5:
+            logging.error('PWM allowed a duty cycle > 100%')
+        else:
+            logging.debug('PWM properly rejected >100% duty cycle')
+            pass
+        pwm.on()
+        if not pwm.enabled:
+            logging.error('The PWM would not turn on')
+        pwm.off()
+        if pwm.enabled:
+            logging.error('The PWM would not shut off')
 
 def safe_int(string):
     '''
